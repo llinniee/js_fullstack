f7c9e4826caa53948af04d08c84b16ae62305a29
llinniee <xlllyyy@163.com>
2019-05-28 15:48:17 +0800

    gupiao

--- lectcod/stock/index.js
+++ lectcod/stock/index.js
@@ -1,2 +1,25 @@
 // 动态规划
 // 有两个状态 买入和卖出状态
+// 只要考虑在当天和之前买哪个收益更高，当天卖和之前卖哪个收益更高
+// 最大收益 交换
+var maxProfit = function (price) {
+  var i, length = price.length,
+    low, high, cur, max= 0;
+    if(length = 0) {
+      return 0;
+    }
+    low = price[0];
+    high = low;
+    for(i = 1; i < length; i++) {
+      cur = price[i];
+      //某一天 要买，low值
+      //要卖 high
+      if(cur > high) {
+        high = cur;
+      }else if (cur < low) {
+        low = cur;
+      }
+    }
+  return max;
+}
+console.log(maxProfit([7, 1, 5, 3, 6, 4]))
