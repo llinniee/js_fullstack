f74bd0bb23ebb6059fd53026c502f6064cb2955b
llinniee <xlllyyy@163.com>
2019-05-29 16:43:39 +0800

    vue

--- node/static-server/readme.md
+++ node/static-server/readme.md
@@ -11,3 +11,12 @@
 ## formidable
   fields 非type= file 最后提交的内容都会到fields
   files type = file
+
+## 静态资源服务
+  双击打开： file:// XXX 本地文件的读取
+  在服务器上：http://localhost:9090/ 访问图片的时候发了一个请求
+  处理static的资源，隐射为请求路径
+  /static/1.png 返回1.png
+  /static/2.png 返回2.png
+  (koa-static)
+  业内有很多成熟的静态资源服务器： Nginx
--- node/static-server/staticServer.js
+++ node/static-server/staticServer.js
@@ -1,9 +1,18 @@
 const http = require('http');
 const fs = require('fs');
 const path = require('path');
+const staticDir = './static/'
 
 http.createServer((req, res) => {
-  console.log(req.url)
+  // console.log(req.url)
+  // koa-static public
+  // static/a.png static/b.png 符合这也规则
+  // ^以什么什么开头
+  const reqUrl = req.url;
+  if(/^\/static\//.test(reqUrl)) {
+    staticServer(req, res);
+    return false;
+  }
   fs.readFile('./static/index.html', 'binary', (err, file) => {
     res.write(file, 'binary')
     res.end();
@@ -11,3 +20,8 @@ http.createServer((req, res) => {
 }).listen(9090, () => {
   console.log('server is running 9090')
 })
+function staticServer(req, res) {
+  let url = req.url
+  const fileName = path.basename(url);
+  const filePath = path.join(__dirname, staticDir, fileName)
+}
