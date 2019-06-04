4c62d8f2278e4387f8f7202cc177940db9dd47d2
llinniee <xlllyyy@163.com>
2019-06-04 15:51:29 +0800

    koa

---
+++ node/koa-template/blog.js
@@ -0,0 +1,10 @@
+const koa = require('koa');
+const app = new koa();
+
+// user 个人主页面
+app.use(async (ctx) => {
+
+})
+app.listen(8080, () => {
+  console.log('server is success 8080')
+})
--- node/koa-template/middleWare-log.js
+++ node/koa-template/middleWare-log.js
@@ -3,10 +3,19 @@ const koa = require('koa');
 const app = new koa();
 const admin = new koa();
 
+app.use(async (ctx, next) => {
+  const start = Date.now();
+  await next();
+  ctx.body = `hello koa`;
+  const end = Date.now();
+  // writeHead() 响应头 content-type
+  ctx.set('X-Response-time', `${end - start}ms`);
+  console.log('spend', `${end - start}ms`);
+})
+app.use(async (ctx) => {
+  ctx.body = `hello koa`;
+})
 app.listen(8080, () => {
   console.log('server is success 8080')
 })
 
-app.use(async (ctx, next) => {
-  
-})
