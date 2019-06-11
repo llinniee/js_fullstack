713a925c36535c05d7ae7a88b2f35ea406419291
llinniee <xlllyyy@163.com>
2019-06-11 21:11:18 +0800

    vue

--- vue/vue-textscroll/textscroll/src/components/textscroll.vue
+++ vue/vue-textscroll/textscroll/src/components/textscroll.vue
@@ -1,8 +1,8 @@
 <template>
   <section>
     <div class="textscroll">
-      <transition-group tag="ul" name="scrolltype">
-        <li v-for="(item, index) in datalists" :key="index" v-show="index === count">
+      <transition-group tag="ul" :name="scrolltype">
+        <li v-for="(item, index) in dataList" :key="index" v-show="index === count">
           <p>{{item.text}}</p>
         </li>
       </transition-group>
@@ -11,46 +11,44 @@
 </template>
 
 <script>
+import { setInterval } from "timers";
 export default {
   data() {
     return {
-      count: 0,
-      datalists: [
-        {text: '坚持'},
-        {text: '不要脸'},
-        {text: '坚持不要脸'},
-      ]
-    }
+      count: 0
+    };
   },
-  props: ['scrolltype'],
+
+  props: ["scrolltype", "dataList"],
+
   created() {
-   setInterval(() => {
-     this.getText()
-   }, 2000);
+    setInterval(() => {
+      this.getText();
+    }, 2000);
   },
   methods: {
     getText() {
-    let len = this.datalists.length;
-    if (len === 0) {
-      return;
-    }
-    if (this.count === len-1 ) {
-      this.count === 0;
-    } else {
-     this.count++
-    }
+      let len = this.dataList.length;
+      if (len === 0) {
+        return;
+      }
+      if (this.count === len - 1) {
+        this.count = 0;
+      } else {
+        this.count++;
+      }
     }
   }
-}
+};
 </script>
 
-<style scoped>
+<style>
 ul {
   margin: 5px 0;
   padding: 0;
-  background-color: #fff;
+  background-color: #ffffff;
   overflow: hidden;
-  height:40px;
+  height: 40px;
 }
 li {
   height: 40px;
@@ -58,17 +56,30 @@ li {
   line-height: 1;
   overflow: hidden;
 }
-  .scroll-up-enter-active {
-    transition: all 1s ease;
-  }
-  .scroll-up-leave-active {
-    transition: all 1s ease;
-  }
-  .scroll-up-enter {
-    transform: translate3d(0, 100%, 0)
-  }
-  .scroll-up-leave {
-    transform: translate3d(0, -100%, 0)
-
-  }
+/* 从上滚动 */
+.scroll-up-enter-active {
+  transition: all 1s ease;
+}
+/* .scroll-up-leave-active {
+  transition: all 1s ease;
+} */
+.scroll-up-enter {
+  transform: translate3d(0, 100%, 0);
+}
+.scroll-up-leave-to {
+  transform: translate3d(0, -100%, 0);
+}
+/* 从左滚动 */
+.scroll-left-enter-active {
+  transition: all 1s ease;
+}
+/* .scroll-left-leave-active {
+  transition: all 1s ease;
+} */
+.scroll-left-enter {
+  transform: translate3d(100%, 0, 0);
+}
+.scroll-left-leave-to {
+  transform: translate3d(-100%, 0, 0);
+}
 </style>
