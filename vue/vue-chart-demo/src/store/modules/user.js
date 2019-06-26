c8504eac61d4f816e5d3a675d84189b8787cbc4c
llinniee <xlllyyy@163.com>
2019-06-26 11:01:01 +0800

    vuex

--- vue/vue-chart-demo/src/store/modules/user.js
+++ vue/vue-chart-demo/src/store/modules/user.js
@@ -1,11 +1,23 @@
+import { resolve } from "path";
+
 const user = {
   state: {
-
+    token: null,
+    avatar: 'https://wpimg.wallstcn.com/3fce7247-d863-4e3d-a0de-d30aaef7358a'
   },
   mutations: {
-
+    SET_TOKEN: (state, token) => {
+      state.token = token;
+    }
   },
-  actions: {}
+  actions: {
+    Login({commit},token) {
+      return new Promise((resolve, reject) => {
+        commit('SET_TOKEN',token)
+        resolve();
+      })
+    }
+  }
 }
 
 export default user
