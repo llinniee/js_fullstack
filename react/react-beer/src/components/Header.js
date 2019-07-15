b6981c0ab403d72df6927e2b819ee2a67e356962
llinniee <xlllyyy@163.com>
2019-07-15 16:41:52 +0800

    react-context

--- react/react-beer/src/components/Header.js
+++ react/react-beer/src/components/Header.js
@@ -1,18 +1,21 @@
-import React from 'react'
+import React from 'react';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
- 
+
 class Header extends React.Component {
-  static PropTypes = {
-    siteName: PropTypes.string
+  static propTypes = {
+    siteName:PropTypes.string
   }
-  render() {
+  state = {  }
+  render() { 
     return (
       <h1>
-        <Link to="/">{this.props.siteName}</Link>
+        <Link to="/">
+          {this.props.siteName}
+        </Link>
       </h1>
-    )
+    );
   }
 }
-
+ 
 export default Header;
--- react/react-beer/src/components/Main.js
+++ react/react-beer/src/components/Main.js
@@ -3,10 +3,21 @@ import Header from './Header.js'
 import Search from './Search.js'
  
 class Main extends React.Component {
+  constructor () {
+    super();
+    this.state = {
+      beers: [],
+      loading: true
+    }
+  }
+
+  componentDidMount() {
+    const params = this.props
+  }
   render() {
     return (
       <div className="wrapper">
-        <Header sideName="Beer me!"/>
+        <Header siteName="Beer me!"/>
         <Search />
         {/* <Results /> */}
       </div>
