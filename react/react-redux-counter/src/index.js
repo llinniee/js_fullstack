65a98fc729cb19d2c8e6de896a8538af4957c4f2
llinniee <xlllyyy@163.com>
2019-07-16 11:57:55 +0800

    add

--- react/react-redux-counter/src/index.js
+++ react/react-redux-counter/src/index.js
@@ -1,12 +1,61 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { createStore, combineReducers } from 'redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+function filmReducer(state = [], action) {
+  switch (action.type) {
+    case 'ADD_FILM':
+      return [...state, action.film];
+    default :
+      return state;
+  }
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+function reducer(state, action) {
+  console.log('reducer->>>>', action);
+  if (action.type === 'INCREMENT') {
+    return state + 1;
+  } else if (action.type === 'DECREMENT') {
+    return state - 1;
+  } else {
+    return 0;
+  }
+}
+const index = combineReducers({
+  count: reducer,
+  films: filmReducer
+})
+
+const store = createStore(index);
+class Count extends Component {
+  state = {}
+  handleIn = () => {
+    store.dispatch({ type: 'INCREMENT'})
+  }
+  handleDe = () => {
+    store.dispatch({ type: 'DECREMENT'})
+  }
+  render() {
+    return (
+       <div>
+         count: {store.getState().count}
+         <button onClick={this.handleIn}>+</button>
+         <button onClick={this.handleDe}>-</button>
+         <button onClick={this.handleAddFilm}>添加电影</button>
+         {
+           store.getState().films.map((film, i) => {
+             return (
+               <li key={i}>{film.name}</li>
+             )
+           })
+         }
+       </div>
+    );
+  }
+}
+ReactDOM.render(<Count />, document.getElementById('root'));
+store.subscribe(() => {
+  ReactDOM.render(<Count />, document.getElementById('root'));
+})
+
+
