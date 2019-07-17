b611dbc173ec193d30afa31862cb77c02ff77ab0
llinniee <xlllyyy@163.com>
2019-07-17 15:18:28 +0800

    yiels

---
+++ interview/deep-async-await/README.md
@@ -0,0 +1,9 @@
+# async 
+async 函数是Generator函数的语法糖
+
+
+通过g.next()一步一步调用
+每一步调用执行到yield关键词
+
+通过传参 可作为上一个yield 语句的返回值
+
--- react/react-redux-counter/src/App.js
+++ react/react-redux-counter/src/App.js
@@ -1,26 +1,51 @@
 import React from 'react';
+import { connect } from 'react-redux'
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const mapStateToProps = (state) => {
+  //
+  return {
+    count: state.count,
+    films: state.films
+  }
+}
+const mapDispatchToProps = (dispatch) => {
+  return {}
+}
+class Count extends React.Component {
+  state = {  }
+  handleIn = () => {
+  //   store.dispatch({ type: 'INCREMENT'})
+  // }
+  handleDe = () => {
+    // store.dispatch({ type: 'DECREMENT'})
+  }
+  handleAddFilm = () => {
+    // store.dispatch({ 
+    //   type: 'ADD_FILM',
+    //   film:{ name:'葫芦娃大战哥斯拉' }
+    // })
+  }
+  render() { 
+    console.log(this.props);
+    const { count, films } = this.props
+    return (
+      <div>
+        count: {count}
+        <button onClick={this.handleIn}>+</button>
+        <button onClick={this.handleDe}>-</button>
+        <button onClick={this.handleAddFilm}>添加电影</button>
+        {
+          films.map((film,i) => {
+            return (
+              <li key={i}>{film.name}</li>
+            )
+          })
+        }
+      </div>
+    );
+  }
 }
 
-export default App;
+export default connect(mapStateToProps,mapDispatchToProps)(Count);
